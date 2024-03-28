863d780513413109e9a98bee7b0ecbdfa90cb1c6
MinhDangph34649 <dangnmph34649@fpt.edu.vn>
2024-03-28 15:54:05 +0700

    minhdang

---
+++ app/src/main/java/com/example/duancho/sua_Thong_tin_nguoi_dung.java
@@ -0,0 +1,24 @@
+package com.example.duancho;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class sua_Thong_tin_nguoi_dung extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_sua_thong_tin_nguoi_dung);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
