d60ac397ddd7f0c46fc353045b3e5966c4d9a4fd
luc244 <Lucnguyen2442001@gmail.com>
2024-04-03 15:42:22 +0700

    dang

---
+++ app/src/main/java/com/example/duancho/Utils/classsiu.java
@@ -0,0 +1,9 @@
+package com.example.duancho.Utils;
+
+
+import com.example.duancho.Model.DonHang;
+import com.example.duancho.Model.DonHangChiTiet;
+public class classsiu {
+    private String Name;
+    DonHang donHang= new DonHang();
+}
